0aafa681ca1200a44fdf9cfd3b46372e38acca6b
Andrew Baik <and.baik@gmail.com>
2016-01-13 20:28:53 -0500

    started move_graph class

--- move_graph.rb
+++ move_graph.rb
@@ -1,3 +1,22 @@
 Move = Struct.new(:x, :y, :linked_nodes)
 
-class
+#implements adjacent list
+class MoveGraph
+  DELTA = [[2,1], [1,2], [-1,2], [-2,1], [-2,-1], [-1,-2], [1,-2], [2,-1]]
+
+  def initialize(coords, max_depth)
+    @start = coords
+    @max_depth = max_depth
+    @array = Array.new(64)
+    @node_counter = 1
+    generate_moves
+  end
+
+  def hash(coord)
+    8 * coord[0] + coord[1]
+  end
+
+  def generate_moves
+    
+  end
+end
